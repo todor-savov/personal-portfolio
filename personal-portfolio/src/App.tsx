772bf728bc8b36950fce55b09e601dcf260f4422
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-11-08 18:43:45 +0200

    Add Particles component to App.tsx from react-particles library and configure it with options

--- personal-portfolio/src/App.tsx
+++ personal-portfolio/src/App.tsx
@@ -1,22 +1,39 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Projects from './components/Projects/Projects'
+import { useCallback } from "react";
+import Particles from 'react-particles';
+import { particlesOptions } from './common/particles';
+import type { Container, Engine } from "tsparticles-engine";
+import { loadSlim } from "tsparticles-slim";
 import Navigation from './components/Navigation/Navigation'
+import Home from './components/Home/Home'
+import Projects from './components/Projects/Projects'
 import './App.css'
 
 const App = () => {
 
+  const particlesInit = useCallback(async (engine: Engine) => {
+    console.log(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container: Container|undefined) => {
+    await console.log(container);
+  }, []);
+     
   return (
     <BrowserRouter>
       <Navigation />
       <Routes>
-        <Route path="/home" element={<h1>Home</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<h1>About</h1>} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/experience" element={<h1>Experience</h1>} />
         <Route path="/contact" element={<h1>Contact</h1>} />
-      </Routes>      
+      </Routes>  
+      <Particles id="tsparticles" init={particlesInit} options={particlesOptions} loaded={particlesLoaded} />    
     </BrowserRouter>
   )
 }
 
-export default App
+export default App;
