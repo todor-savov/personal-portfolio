e72e9d10f9d49f1911582faf177793f3708388c2
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-11-08 14:15:02 +0200

    Add the data for all projects to src/common/constants.ts (as array of objects)

--- personal-portfolio/src/common/constants.ts
+++ personal-portfolio/src/common/constants.ts
@@ -1,26 +1,51 @@
-const projects = [
+export const projects = [
     {   
         title: 'Personal Finance Management App', 
         description: `  Allows users to register transactions, attach receipts, and edit entries. 
                         It features a dynamic table with filtering and sorting options and users can visualize their finances with pie charts and graphs. 
                         Budgeting and tracking monthly limits are also supported.        
                     `,
-        image: '/finance-app-photo.jpeg',
+        image: '/finance_app_photo.jpg',
         github: 'https://github.com/todor-savov/Expense-Tracker',
         liveSite: 'https://expense-tracking-app-33495.web.app/',
-        techStack: ['React', 'TypeScript', 'Material UI', 'CSS', 'Firebase']
+        techStack: ['React', 'TypeScript', 'JavaScript', 'Material UI', 'CSS', 'Firebase']
     },
     {   
         title: 'Dynamic Event Scheduling Web App', 
         description: `  A feature-rich event calendar web app for managing and organizing personal events daily, weekly, and monthly. 
                         The app enables event creation, invitations, and seamless collaboration, with admin functionality for managing users and events.        
                     `,         
-        image: '/event-calendar-app-photo.jpeg',
+        image: '/event_calendar_app_photo.jpg',
         github: 'https://github.com/React-Duo/Event-Calendar',
         liveSite: 'https://event-calendar-c74a9.web.app/',
-        techStack: ['React', 'JavaScript', 'CSS', 'Firebase']
+        techStack: ['React', 'JavaScript', 'HTML5', 'CSS', 'Firebase']
     },
-    
-];
+    {
+        title: 'Online Discussion Forum for React Enthusiasts',
+        description:`   A dynamic and responsive forum application with an intuitive user interface, 
+                        real-time data synchronisation and secure user authentication with Firebase.
+                    `,
+        image: '/react_forum_photo.jpg',
+        github: 'https://github.com/React-Duo/Forum-Project',
+        liveSite: 'https://reactduo-forumproject.web.app/',
+        techStack: ['React', 'JavaScript', 'HTML5', 'CSS', 'Firebase']
+    },
+    {
 
-export { projects };
+        title: 'GIF Search and Browse Site',
+        description: `  Developed a Giphy website using vanilla JavaScript and the Giphy API to showcase trending GIFs. 
+                        The site allows users to upload and favorite their own GIFs, with local storage functionality to manage favorites and enhance the user experience.
+                     `,
+        image: '/no_preview_image.jpg',
+        github: 'https://github.com/Cyber-Creators/WebProject_1',
+        techStack: ['JavaScript', 'HTML5', 'CSS', 'Web Storage API']
+    },    
+    {
+        title: 'Remote Resource Monitoring Desktop App',
+        description: `  Created a client-server network app for admins to monitor CPU, RAM, and disk usage of remote Windows computers via a platform-independent UI. 
+                        It helps with decision-making for upgrading or replacing LAN resources.
+                     `,
+        image: '/no_preview_image.jpg',
+        techStack: ['Java Swing', 'JDBC', 'MySQL', 'Windows Management Instrumentation (WMI)']
+    }    
+];
