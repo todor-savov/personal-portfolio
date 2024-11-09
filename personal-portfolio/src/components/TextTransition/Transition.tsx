8100d292b0a9746492374d4583c79a6e34c39ff3
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-11-09 21:32:41 +0200

    Create Transition.tsx component to handle the tech skills text transitions in Home page

---
+++ personal-portfolio/src/components/TextTransition/Transition.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import TextTransition, { presets } from 'react-text-transition';
+import { Box, Typography } from "@mui/material";
+import { SiReact, SiJavascript, SiTypescript } from 'react-icons/si';
+
+const Transition = () => {
+    const [index, setIndex] = useState(0);
+
+    const techSkills = [
+        "React",
+        "JavaScript",
+        "TypeScript",  
+    ];
+
+    const icons = [
+        <SiReact size={40} color="#61DAFB" />,
+        <SiJavascript size={40} color="#f0db4f" />,
+        <SiTypescript size={40} color="#007acc" />,         
+    ];
+
+    useEffect(() => {
+        const intervalId = setInterval(() => setIndex((index) => index + 1), 3000);
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return (
+        <Typography variant="h5" style={{ display: 'flex', fontWeight: 'bold', fontSize: '2.3rem' }}>
+            I am a Front-End Developer with&nbsp;
+            <Box style={{ display: 'flex', alignItems: 'center', minWidth: '250px' }}>
+                {icons[index % icons.length]}&nbsp;
+                <span style={{ color: "#1976d2" }}>
+                    <TextTransition springConfig={presets.wobbly}>
+                        {techSkills[index % techSkills.length]}
+                    </TextTransition>
+                </span>
+            </Box>
+        </Typography>
+    );
+};
+
+export default Transition;
