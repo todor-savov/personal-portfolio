d8389318b5024f1243dba9287e24b9aa3d12928a
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-11-10 21:49:23 +0200

    Add animation to the intro text in About.tsx

--- personal-portfolio/src/components/About/About.tsx
+++ personal-portfolio/src/components/About/About.tsx
@@ -1,6 +1,7 @@
 import { Box, Typography, Avatar } from '@mui/material';
 import { FaReact, FaJsSquare, FaDatabase } from 'react-icons/fa';
 import { SiTypescript, SiFirebase, SiMaterialdesignicons } from 'react-icons/si';
+import AnimatedText from "react-animated-text-content";
 import ownPhoto from '../../assets/IMG_0004-Photoroom.png';
 import './About.css';
 
@@ -21,28 +22,40 @@ const About = () => {
             <Avatar alt="Todor-Savov" src={ownPhoto} variant='square' id='avatar-photo' />
 
             <Box className="text-container">
-                <Typography variant="h4" gutterBottom sx={{fontWeight: 'bold'}}> ABOUT ME </Typography>
-                
-                <Typography variant="body1" color="text.secondary" sx={{textAlign: 'justify'}}>
+                <Typography variant="h4" gutterBottom sx={{ fontWeight: "bold" }}> ABOUT ME </Typography>
+
+                <AnimatedText 
+                        type="chars"
+                        animation={{
+                            x: "200px",
+                            y: "-20px",
+                            scale: 1.1,
+                            ease: "ease-in-out",             
+                        }}
+                        animationType="float"
+                        interval={0.02}
+                        duration={0.3}
+                        includeWhiteSpaces
+                        threshold={0.1}
+                        rootMargin="20%"
+                >                    
                     I am a versatile front-end developer with a unique blend of experience in business, finance, and technology. 
                     My journey began with a Master's in European Business and Finance from Nottingham Business School, which provided 
                     me with a solid understanding of the business landscape. Driven by a passion for technology, I later pursued a 
                     second Master’s degree in Distributed Systems and Mobile Technologies at Sofia University St. Kliment Ohridski, 
                     where I discovered my enthusiasm for programming and problem-solving.
-                </Typography>
 
-                <Typography variant="body1" color="text.secondary" sx={{textAlign: 'justify'}}>
                     My transition into front-end development began with hands-on projects, including creating dynamic, user-focused 
                     applications using React, TypeScript, and Firebase. Alongside my technical expertise, my background in technical 
                     support at SiteGround honed my skills in troubleshooting, attention to detail, and effective communication. 
                     Today, I am passionate about building interactive, responsive applications and continuously expanding my skills in 
                     JavaScript and front-end technologies.
-                </Typography>
+                </AnimatedText>                   
             </Box>
         </Box>
 
         <Box className="second-container">
-            <Typography variant="h5" gutterBottom id='tech-stack'> Tech Stack </Typography>
+            <Typography variant="h5" gutterBottom id='tech-stack'> Tech Skills </Typography>
         
             <div id='container-scroll'>
                 <div className='scroll'>
