3794dd840b86bb80af3a1e29e4fb5e458ddb897f
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-11-13 19:01:24 +0200

    Update Home.tsx to remove the react-animated-text-content import from it as the library is no longer in use

--- personal-portfolio/src/components/Home/Home.tsx
+++ personal-portfolio/src/components/Home/Home.tsx
@@ -1,40 +1,23 @@
 import { Box, IconButton } from "@mui/material";
-import AnimatedText from "react-animated-text-content";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
 import Transition from "../TextTransition/Transition";
+import AnimatedText from "../AnimatedText/AnimatedText";
 import logoAnimation from "../../assets/gummy-app-development.png";
 import "./Home.css";
 
 const Home = () => {
+    
     const welcomeText = [
         "Hello, I'm Todor Savov.",
         "Welcome to my portfolio."
     ];
       
     return (
-        <Box className="home">
+        <Box className="home">            
             {welcomeText.map((text, index) => (
-                <AnimatedText 
-                    key={index}
-                    type="chars"
-                    animation={{
-                        x: "200px",
-                        y: "-20px",
-                        scale: 1.1,
-                        ease: "ease-in-out",             
-                    }}
-                    animationType="float"
-                    interval={0.05}
-                    duration={0.8}
-                    includeWhiteSpaces
-                    threshold={0.1}
-                    rootMargin="20%"
-                    className="animated-text"
-                >
-                    {text}                
-                </AnimatedText>
+                <AnimatedText key={index} text={text} />
             ))}
 
             <Transition />
