52ddf2a91887604af1020480a46362c5a5dc43d0
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-11-13 20:07:43 +0200

    Update AnimatedText component (AnimatedText.tsx) to change the animation effect on text

--- personal-portfolio/src/components/AnimatedText/AnimatedText.tsx
+++ personal-portfolio/src/components/AnimatedText/AnimatedText.tsx
@@ -1,9 +1,24 @@
-import { useSpring, animated } from 'react-spring';
+import { useSprings, animated } from 'react-spring';
 
 const AnimatedText = ({ text } : { text: string }) => {
-  const props = useSpring({ opacity: 1, from: { opacity: 0 }, config: { duration: 1000 } });
   
-  return <animated.div style={props}>{text}</animated.div>;
-};
+  const springs = useSprings(
+    text.length,
+    text.split('').map((_, i) => ({
+        opacity: 1,
+        from: { opacity: 0 },
+        delay: i * 100,
+    }))
+  );
+
+return (
+    <h1>
+        {springs.map((spring, index) => (
+            <animated.span key={index} style={spring}>
+                {text}
+            </animated.span>
+        ))}
+    </h1>
+);};
 
 export default AnimatedText;
