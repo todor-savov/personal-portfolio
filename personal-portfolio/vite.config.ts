b0c2f259d3a56bd4d0b8e96c2a90fbd6a0c1582a
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-11-16 20:10:14 +0200

    Testing with vercel and vite configuration

--- personal-portfolio/vite.config.ts
+++ personal-portfolio/vite.config.ts
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html'
+    }
+  }
 })
