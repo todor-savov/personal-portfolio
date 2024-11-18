69b43440e4ffaf4cc7587749f8b0b25285ae4543
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-11-18 19:30:40 +0200

    Add animation for the main container in Home.tsx (wrap it up with motion.div element from framer-motion library)

--- personal-portfolio/src/components/Home/Home.tsx
+++ personal-portfolio/src/components/Home/Home.tsx
@@ -4,6 +4,7 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
 import Transition from "../TextTransition/Transition";
 import logoAnimation from "../../assets/gummy-app-development.png";
+import { motion } from "framer-motion";
 import "./Home.css";
 
 const Home = () => {
@@ -14,54 +15,61 @@ const Home = () => {
     ];
       
     return (
-        <Box className="home">
-           {welcomeText.map((text, index) => (
-                <div key={index} className="typewriter">
-                    <Typography variant="h4" key={index} sx={{ mb: 2, fontWeight: "bold", mt: index === 0 ? 0 : 2 }}>
-                        {text.split('').map((char, index) => (
-                            <span key={index} style={{ animationDelay: `${index * 0.05}s` }}
-                                className="typewriter-character"
-                            >
-                                {char === ' ' ? '\u00A0' : char}
-                            </span>
-                        ))}
-                    </Typography>
-                </div>
+        <motion.div
+            initial={{ opacity: 0, scale: 0.1, rotate: -180 }}
+            whileInView={{ opacity: 1, scale: 1, rotate: 0 }}
+            viewport={{ once: true }} transition={{ duration: 3 }}
+            style={{ position: 'relative', zIndex: 1 }}
+        >
+            <Box className="home">
+            {welcomeText.map((text, index) => (
+                    <div key={index} className="typewriter">
+                        <Typography variant="h4" key={index} sx={{ mb: 2, fontWeight: "bold", mt: index === 0 ? 0 : 2 }}>
+                            {text.split('').map((char, index) => (
+                                <span key={index} style={{ animationDelay: `${index * 0.05}s` }}
+                                    className="typewriter-character"
+                                >
+                                    {char === ' ' ? '\u00A0' : char}
+                                </span>
+                            ))}
+                        </Typography>
+                    </div>
             ))}
 
-            <Transition />
- 
-            <img src={logoAnimation} alt="logo" className="logo-animation" />
-            <span id="copyrights">Illustration by <a href="https://icons8.com/illustrations/author/SH5qVUiWnjy4">Vijay Verma Ouch!</a></span>
+                <Transition />
+    
+                <img src={logoAnimation} alt="logo" className="logo-animation" />
+                <span id="copyrights">Illustration by <a href="https://icons8.com/illustrations/author/SH5qVUiWnjy4">Vijay Verma Ouch!</a></span>
 
-            <Box className="social-icons" sx={{ mt: 2 }}>
-                <IconButton
-                    component="a"
-                    href="https://github.com/todor-savov"
-                    target="_blank"
-                    aria-label="GitHub"
-                    rel="noopener noreferrer"
-                >
-                    <GitHubIcon fontSize="large" />
-                </IconButton>
-                <IconButton
-                    component="a"
-                    href="https://www.linkedin.com/in/todor-savov-4a14253b/"
-                    target="_blank"
-                    aria-label="LinkedIn"
-                    rel="noopener noreferrer"
-                >
-                    <LinkedInIcon fontSize="large" />
-                </IconButton>
-                <IconButton
-                    component="a"
-                    href="mailto:todor.savov@abv.bg"
-                    aria-label="Email"
-                >
-                    <EmailIcon fontSize="large" />
-                </IconButton>
-            </Box>        
-        </Box>
+                <Box className="social-icons" sx={{ mt: 2 }}>
+                    <IconButton
+                        component="a"
+                        href="https://github.com/todor-savov"
+                        target="_blank"
+                        aria-label="GitHub"
+                        rel="noopener noreferrer"
+                    >
+                        <GitHubIcon fontSize="large" />
+                    </IconButton>
+                    <IconButton
+                        component="a"
+                        href="https://www.linkedin.com/in/todor-savov-4a14253b/"
+                        target="_blank"
+                        aria-label="LinkedIn"
+                        rel="noopener noreferrer"
+                    >
+                        <LinkedInIcon fontSize="large" />
+                    </IconButton>
+                    <IconButton
+                        component="a"
+                        href="mailto:todor.savov@abv.bg"
+                        aria-label="Email"
+                    >
+                        <EmailIcon fontSize="large" />
+                    </IconButton>
+                </Box>        
+            </Box>
+        </motion.div>
     );
 };
 
